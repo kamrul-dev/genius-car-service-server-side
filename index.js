6b2e6495fc469f914cb684613d86f3b26a7fe30e
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-21 23:12:25 +0600

    delete data from database using delete api

--- index.js
+++ index.js
@@ -42,6 +42,15 @@ async function run() {
             const result = await serviceCollection.insertOne(newService);
             res.send(result);
 
+        });
+
+        // delete data from database 
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+
         })
 
     }
