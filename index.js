b4b2571ca7d4d6c1986232783a2829186e4b25cb
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-25 00:58:34 +0600

    Send jwt token in the server, verify and decode jwt token

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyjWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+    })
+    next();
+}
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nqpxv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -66,12 +82,18 @@ async function run() {
 
         // order collection api
 
-        app.get('/order', async (req, res) => {
+        app.get('/order', verifyjWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            const query = { email: email };
-            const cursor = orderColletion.find(query);
-            const orders = await cursor.toArray();
-            res.send(orders);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = orderColletion.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            }
+            else{
+                res.status(403).send({message: 'forbidden access'})
+            }
         })
 
         app.post('/order', async (req, res) => {
