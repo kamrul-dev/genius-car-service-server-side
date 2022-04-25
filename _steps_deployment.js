37857e2d2d3b1a9c6264c752ff3a5c83198fe382
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-25 20:37:53 +0600

    steps added

---
+++ _steps_deployment.js
@@ -0,0 +1,14 @@
+/**
+ * -------------------------
+ * ONE TIME FOR YOUR COMPUTER
+ * ----------------------------
+ * 1. Create heroku account
+ * 2. varify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * ------------------------------
+ * 
+ * FOR EACTH PROJECT ONE TIME
+ * -----------------------------
+ * 1. 
+*/
